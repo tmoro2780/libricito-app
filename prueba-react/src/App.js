b3f64c9fa4881791a24804cb30fc6d7badffd2fc
Fran <212407299+tmoro2780@users.noreply.github.com>
2025-05-27 21:31:29 -0300

    Empezando a trabajar en React

---
+++ prueba-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navegador } from './components/navbar.js';
+
+function App() {
+  return (
+    <div className="App">
+        <Navegador />
+    </div>
+  );
+}
+
+export default App;
